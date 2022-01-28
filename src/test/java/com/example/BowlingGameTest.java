e662cb16d8c43b94119fc437c253972f753f3670
Ahsanadam <adamebadul@gmail.com>
2022-01-28 14:16:55 +0100

    Add class BowlingGame and add first test for that class

---
+++ src/test/java/com/example/BowlingGameTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+
+    @Test
+    void canCreateABowlingGame(){
+        new BowlingGame();
+
+    }
+
+}
